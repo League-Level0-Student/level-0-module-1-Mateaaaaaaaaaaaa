1f941eb762800c711dffbc27c425c54d5712427e
student <student@192.168.1.54>
2023-08-29 14:02:38 -0700

    loop practrice

---
+++ src/_05_for_loops/_1_my_ages/Practice.java
@@ -0,0 +1,13 @@
+package _05_for_loops._1_my_ages;
+
+public class Practice {
+	public static void main(String[] args) {
+		for(int matea=100; matea<=1000;matea+=100) {
+			System.out.println(matea);
+		}
+
+		for(int matea=1000; matea>=100;matea-=100) {
+			System.out.println(matea);
+		}
+	}
+}
